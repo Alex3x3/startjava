ce93d9d1f2729de8dcba7b24f3992b0bd3ac8445
AlexP <tony.qwerin@gmail.com>
2022-09-01 11:37:27 +0300

    Lesson 1 final tasks has been added

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+        char sign = '^';
+        int result = 0;
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/') {
+            result = num1 / num2;
+        } else if (sign == '^') {
+            result = num1;
+            for (int i = 0; i < num2 - 1; i++) {
+                result *= num1;
+            }
+        } else if (sign == '%') {
+            result = num1 % num2;
+        }
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
