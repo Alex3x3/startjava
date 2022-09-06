8e3574a483f9562b55f3c90916a118303defd346
AlexP <tony.qwerin@gmail.com>
2022-09-06 13:36:23 +0300

    Person.java is added

---
+++ Lesson2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+
+    String name = "Иван";
+    char sex = 'M';
+    double height = 1.86;
+    int weight = 80;
+
+    void walk() {
+        System.out.println(name + " идет");
+    }
+
+    void sit() {
+        System.out.println(name + " сидит");
+    }
+
+    void run() {
+        System.out.println(name + " бежит");
+    }
+
+    void talk() {
+        System.out.println(name + " говорит");
+    }
+
+    void learnJava() {
+        System.out.println(name + " изучает Java");
+    }
+}
