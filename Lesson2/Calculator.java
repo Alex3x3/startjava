e10af10acc3f2ef796be5aef118b9bc6da3802a6
AlexP <tony.qwerin@gmail.com>
2022-09-08 20:10:02 +0300

    Lesson2 Calculator and CalculatorTest modified after review

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -1,9 +1,37 @@
 public class Calculator {
 
-    int num1;
-    int num2;
-    char sign;
-    int result;
+    private int num1;
+    private int num2;
+    private char sign;
+    private int result;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public char getSign() {
+        return sign;
+    }
+
+    public void setSign(char sign) {
+        this.sign = sign;
+    }
+
+    public int getResult() {
+        return result;
+    }
 
     public void calculate() {
         switch (sign) {
@@ -23,8 +51,8 @@ public class Calculator {
                 result = num1 % num2;
                 break;
             case '^':
-                result = num1;
-                for (int i = 0; i < num2 - 1; i++) {
+                result = 1;
+                for (int i = 0; i < num2; i++) {
                     result *= num1;
                 }
                 break;
--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -3,32 +3,25 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-        System.out.println("\n###Программа Калькулятор v0.99 beta###");
+        System.out.println("\n###Программа Калькулятор v1.0###");
         Calculator calculator = new Calculator();
-        boolean repeatCalculation = true;
-        while (repeatCalculation) {
+        String userAnswer = "yes";
+        while (userAnswer.equals("yes")) {
             Scanner input = new Scanner(System.in);
             System.out.print("\nВведите первое число: ");
-            calculator.num1 = input.nextInt();
+            calculator.setNum1(input.nextInt());
             System.out.print("Введите знак математической операции: ");
-            calculator.sign = input.next().charAt(0);
+            calculator.setSign(input.next().charAt(0));
             System.out.print("Введите второе число: ");
-            calculator.num2 = input.nextInt();
+            calculator.setNum2(input.nextInt());
             calculator.calculate();
-            System.out.print(calculator.num1 + " " + calculator.sign + " " + calculator.num2 +
-                    " = " + calculator.result);
+            System.out.print(calculator.getNum1() + " " + calculator.getSign() + " " +
+                    calculator.getNum2() + " = " + calculator.getResult());
             input.nextLine();
-
-            while (true) {
+            do {
                 System.out.print("\nХотите продолжить вычисления? [yes/no]: ");
-                String userAnswer = input.nextLine();
-                if (userAnswer.equals("no")) {
-                    repeatCalculation = false;
-                    break;
-                } else if (userAnswer.equals("yes")) {
-                    break;
-                }
-            }
+                userAnswer = input.nextLine();
+            } while (!userAnswer.equals("yes") && !userAnswer.equals("no"));
         }
     }
 }
