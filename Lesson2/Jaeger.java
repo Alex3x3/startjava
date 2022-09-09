8da3da318d58ba5cb5eef5acf67e6002827b2ac2
AlexP <tony.qwerin@gmail.com>
2022-09-09 17:01:53 +0300

    Jaeger and JaegerTest added

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,97 @@
+public class Jaeger {
+
+    private String modelName;
+    private String weapon;
+    private String powerMove;
+    private int armor;
+    private float power;
+    private int ammo;
+    private boolean alive;
+
+    public Jaeger() {
+        alive = true;
+    }
+
+    public Jaeger(String modelName, String weapon, String powerMove,
+                  int armor, float power, int ammo) {
+        this.modelName = modelName;
+        this.weapon = weapon;
+        this.powerMove = powerMove;
+        this.armor = armor;
+        this.power = power;
+        this.ammo = ammo;
+        alive = true;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public String getPowerMove() {
+        return powerMove;
+    }
+
+    public void setPowerMove(String powerMove) {
+        this.powerMove = powerMove;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        if (armor >= 0) {
+            this.armor = armor;
+        }
+    }
+
+    public float getPower() {
+        return power;
+    }
+
+    public void setPower(float power) {
+        if (power >= 0) {
+            this.power = power;
+        }
+    }
+
+    public int getAmmo() {
+        return ammo;
+    }
+
+    public void setAmmo(int ammo) {
+        if (ammo >= 0) {
+            this.ammo = ammo;
+        }
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        if (this.alive) {
+            this.alive = alive;
+        }
+    }
+
+    public void move() {
+        System.out.print(modelName + " makes a powermove: ");
+    }
+
+    public void strike() {
+        System.out.print(modelName + " strikes kaiju with ");
+    }
+}
---
+++ Lesson2/JaegerTest.java
@@ -0,0 +1,55 @@
+public class JaegerTest {
+
+    public static void main(String[] args) {
+        System.out.println("\n###Battle with kaiju###\n");
+        Jaeger jaeger1 = new Jaeger();
+        jaeger1.setModelName("Crimson Typhoon");
+        jaeger1.setWeapon("IB22 Plasmacaster");
+        jaeger1.setPowerMove("Jet Kick");
+        jaeger1.setArmor(6);
+        jaeger1.setPower(98.6F);
+        jaeger1.setAmmo(20);
+
+        jaeger1.move();
+        System.out.println(jaeger1.getPowerMove());
+        jaeger1.setPower(86.3F);
+        System.out.println(jaeger1.getModelName() + " power level: " + jaeger1.getPower() + " %");
+
+        jaeger1.strike();
+        System.out.println(jaeger1.getWeapon());
+        jaeger1.setAmmo(17);
+        System.out.println(jaeger1.getModelName() + " ammo level: " + jaeger1.getAmmo() + " units");
+
+        System.out.println(jaeger1.getModelName() + " is hit by kaiju");
+        System.out.println(jaeger1.getModelName() + " armor level: " + jaeger1.getArmor() +
+                " units");
+
+        Jaeger jaeger2 = new Jaeger("Striker Eureka", "Assault Mount 3.25 \"Sting-Blades\"",
+                "Six Shooter", 9, 76.4F, 10);
+        System.out.println("\nThe second jaeger - " + jaeger2.getModelName() +
+                ", joins the battle\n");
+
+        jaeger2.move();
+        System.out.println(jaeger2.getPowerMove());
+        jaeger2.setPower(63.5F);
+        System.out.println(jaeger2.getModelName() + " power level: " + jaeger2.getPower() + " %");
+
+        jaeger2.strike();
+        System.out.println(jaeger2.getWeapon());
+        jaeger2.setAmmo(6);
+        System.out.println(jaeger2.getModelName() + " ammo level: " + jaeger2.getAmmo() + " units");
+
+        System.out.println("\nKaiju bites " + jaeger1.getModelName());
+        jaeger1.setArmor(0);
+        System.out.println(jaeger1.getModelName() + " armor level: " + jaeger1.getArmor() +
+                " units");
+        jaeger1.setAlive(false);
+        System.out.println(jaeger1.getModelName() + " alive status: " + jaeger1.isAlive());
+        System.out.println();
+
+        jaeger2.setWeapon("WMB2x90 AKM Chest launcher");
+        jaeger2.strike();
+        System.out.println(jaeger2.getWeapon());
+        System.out.println("Kaiju is killed by " + jaeger2.getModelName());
+    }
+}
