954acd9a63574f6789997e52cecd15f28dec18dd
AlexP <tony.qwerin@gmail.com>
2022-09-10 13:28:40 +0300

    GuessNumber and GuessNumberTest changed after review

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -2,7 +2,41 @@ import java.util.Scanner;
 
 public class GuessNumber {
 
-    public void startRound(Player player, int guessNum) {
+    private Player player1;
+    private Player player2;
+    private int guessNum;
+
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void startGame() {
+        Scanner input = new Scanner(System.in);
+        String gameContinue = "yes";
+        while (gameContinue.equals("yes")) {
+            guessNum = (int) (Math.random() * 100 + 1);
+            do {
+                startRound(player1);
+                if (player1.getNumber() == guessNum) {
+                    break;
+                }
+
+                startRound(player2);
+                if (player2.getNumber() == guessNum) {
+                    break;
+                }
+            } while (player1.getNumber() != guessNum &&
+                    player2.getNumber() != guessNum);
+
+            do {
+                System.out.print("\nХотите продолжить игру? [yes/no]: ");
+                gameContinue = input.nextLine();
+            } while (!gameContinue.equals("yes") && !gameContinue.equals("no"));
+        }
+    }
+
+    private void startRound(Player player) {
         Scanner input = new Scanner(System.in);
         System.out.println("\nОчередь игрока: " + player.getName());
         System.out.print("Введите ваш ответ: ");
--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -2,16 +2,6 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
 
-    private Player player1;
-    private Player player2;
-    private int guessNum;
-
-    public GuessNumberTest(Player player1, Player player2) {
-        this.player1 = player1;
-        this.player2 = player2;
-        guessNum = (int) (Math.random() * 100 + 1);
-    }
-
     public static void main(String[] args) {
         System.out.println("\n###Guess Number Game v0.98 beta###\n");
         System.out.print("Введите имя первого игрока: ");
@@ -19,28 +9,7 @@ public class GuessNumberTest {
         Player player1 = new Player(input.nextLine());
         System.out.print("Введите имя второго игрока: ");
         Player player2 = new Player(input.nextLine());
-
-        String gameContinue = "yes";
-        while (gameContinue.equals("yes")) {
-            GuessNumberTest game = new GuessNumberTest(player1, player2);
-            GuessNumber round = new GuessNumber();
-            do {
-                round.startRound(game.player1, game.guessNum);
-                if (game.player1.getNumber() == game.guessNum) {
-                    break;
-                }
-
-                round.startRound(game.player2, game.guessNum);
-                if (game.player2.getNumber() == game.guessNum) {
-                    break;
-                }
-            } while (game.player1.getNumber() != game.guessNum &&
-                game.player2.getNumber() != game.guessNum);
-
-            do {
-                System.out.print("\nХотите продолжить вычисления? [yes/no]: ");
-                gameContinue = input.nextLine();
-            } while (!gameContinue.equals("yes") && !gameContinue.equals("no"));
-        }
+        GuessNumber guessNumber = new GuessNumber(player1, player2);
+        guessNumber.startGame();
     }
 }
