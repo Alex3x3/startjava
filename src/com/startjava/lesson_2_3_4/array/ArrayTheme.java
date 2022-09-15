90eda45cf48856f1cd9e05944b15ac2fff2904fd
AlexP <tony.qwerin@gmail.com>
2022-09-15 13:32:09 +0300

    ArrayTheme modified after review

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -7,44 +7,42 @@ public class ArrayTheme {
         System.out.println("\nЗадание 1. Реверс значений массива\n");
         int[] intArr = {2, 3, 7, 1, 5, 4, 6};
         System.out.println("Массив до реверсирования:");
-        ArrayTheme output = new ArrayTheme();
-        output.showIntArr(intArr);
+        ArrayTheme arrayTheme = new ArrayTheme();
+        arrayTheme.showIntArr(intArr);
         System.out.println();
 
-        int[] intArrRev = new int[intArr.length];
-        for (int i = intArrRev.length - 1, j = 0; i >= 0; i--, j++) {
-            intArrRev[i] = intArr[j];
+        for (int i = 0, j = intArr.length - 1; i < intArr.length / 2; i++, j--) {
+            int tmp = intArr[i];
+            intArr[i] = intArr[j];
+            intArr[j] = tmp;
         }
         System.out.println("\nМассив после реверсирования:");
-        output.showIntArr(intArrRev);
+        arrayTheme.showIntArr(intArr);
 
         System.out.println("\n\nЗадание 2. Вывод произведения элементов массива\n");
-        intArr = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        String expression = "";
-        String unusedElements = "";
-        int result = 1;
-        for (int i = 0; i < intArr.length; i++) {
-            expression += (intArr[i] != 0 && intArr[i] != 8 && intArr[i] != 9) ? (intArr[i] + " * ")
-                    : intArr[i] == 8 ? (intArr[i] + " = ")
-                    : ("");
-            result *= (intArr[i] != 0 && intArr[i] != 9) ? intArr[i] : 1;
-            unusedElements += (intArr[i] == 0 || intArr[i] == 9) ?
-                    ("Индекс " + intArr[i] + " = "  + i + "\n")
-                    : "";
+        intArr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            intArr[i] = i;
         }
-        System.out.println(expression + result);
-        System.out.println(unusedElements);
 
-        System.out.println("Задание 3. Удаление элементов массива\n");
+        int result = 1;
+        for (int i = 1; i < intArr.length - 1; i++) {
+            result *= intArr[i];
+            System.out.print(intArr[i] != 8 ? (intArr[i] + " * ")
+                    : intArr[i] == 8 ? (intArr[i] + " = " + result)
+                    : (""));
+        }
+        System.out.println("\n\n" + intArr[0] + " " + intArr[9]);
+
+        System.out.println("\nЗадание 3. Удаление элементов массива\n");
         double[] doubleArr = new double[15];
         for (int i = 0; i < doubleArr.length; i++) {
             doubleArr[i] = Math.random();
         }
         System.out.println("Исходный массив:\n");
-        output.showDoubleArr(doubleArr);
+        arrayTheme.showDoubleArr(doubleArr);
 
-        int middleCellIndex = (doubleArr.length - 1) / 2;
-        double middleCellValue = doubleArr[middleCellIndex];
+        double middleCellValue = doubleArr[(doubleArr.length - 1) / 2];
         int zeroedCellCount = 0;
         for (int i = 0; i < doubleArr.length; i++) {
             if (doubleArr[i] > middleCellValue) {
@@ -53,7 +51,7 @@ public class ArrayTheme {
             }
         }
         System.out.println("\nИзмененный массив:\n");
-        output.showDoubleArr(doubleArr);
+        arrayTheme.showDoubleArr(doubleArr);
         System.out.println("\nколичество обнуленных ячеек: " + zeroedCellCount);
 
         System.out.println("\nЗадание 4. Вывод элементов массива лесенкой в обратном порядке\n");
@@ -65,7 +63,7 @@ public class ArrayTheme {
         int elementsInRow = 1;
         int currentPos = 0;
         while (elementsInRow <= chArray.length) {
-            for (int i = chArray.length - 1; i >= 0 ; i--) {
+            for (int i = chArray.length - 1; i >= 0; i--) {
                 System.out.print(chArray[i]);
                 currentPos++;
 
@@ -106,7 +104,7 @@ public class ArrayTheme {
         System.out.println("\nЗадание 6. Сдвиг элементов массива\n");
         String[] srcStringArr = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
         System.out.print("Исходный массив: ");
-        output.showStringArr(srcStringArr);
+        arrayTheme.showStringArr(srcStringArr);
         System.out.println();
 
         int count = 0;
@@ -131,7 +129,7 @@ public class ArrayTheme {
             }
         }
         System.out.print("Преобразованный массив: ");
-        output.showStringArr(destStringArr);
+        arrayTheme.showStringArr(destStringArr);
         System.out.println();
     }
 
